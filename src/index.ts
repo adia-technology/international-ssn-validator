7c238444de327b4a3b18d4abe9d62f3d39d4877c
Michał Dudak <michal@dudak.me>
2020-04-06 14:38:47 +0200

    Exported CountryCode, added additional error handling

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,20 @@
 import { validateSwissSsn } from './validateSwissSsn';
 import { validateUsSsn } from './validateUsSsn';
 
-type CountryCode = 'us' | 'ch';
+export type CountryCode = 'us' | 'ch';
 
 export function validateSsn(ssn : string, countryCode : CountryCode) : boolean {
-    switch (countryCode) {
-        case 'us':
-            return validateUsSsn(ssn);
-        case 'ch':
-            return validateSwissSsn(ssn);
-        default:
-            throw new Error('');
-    }
+
+	if (countryCode == null || countryCode as string === '') {
+		throw new Error('countryCode was not provided');
+	}
+
+	switch (countryCode) {
+		case 'us':
+			return validateUsSsn(ssn);
+		case 'ch':
+			return validateSwissSsn(ssn);
+		default:
+			throw new Error(`'${countryCode}' country code is not supported.`);
+	}
 }
--- src/validateSwissSsn.ts
+++ src/validateSwissSsn.ts
@@ -3,10 +3,10 @@ import AhvValidator from 'ahv13-validator';
 const ahvRegex = /^756\.\d{4}\.\d{4}\.\d{2}$/;
 
 export function validateSwissSsn(ssn ?: string) : boolean {
-    if (ssn == null || !ahvRegex.test(ssn)) {
-        return false;
-    }
+	if (ssn == null || !ahvRegex.test(ssn)) {
+		return false;
+	}
 
-    let ahvValidator = new AhvValidator();
-    return ahvValidator.isValid(ssn);
+	let ahvValidator = new AhvValidator();
+	return ahvValidator.isValid(ssn);
 }
